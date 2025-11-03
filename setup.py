78018ca7cf13eee846a206dd2fc200a8dad25f91
christpaul94 <christpaul94@gmail.com>
2025-11-03 19:25:14 +0100

    Create setup.py for trapped_atoms_simulation
    Added setup configuration for trapped_atoms_simulation package.

--- setup.py
+++ setup.py
@@ -1,1 +1,19 @@
-
+setup(
+    name='trapped_atoms_simulation', # Name für pip
+    version='0.1.0',
+    author='Paul Christ', # (Oder Ihr Name)
+    description='Simulationscode für meine Masterarbeit',
+    
+    # Diese Zeile ist entscheidend:
+    # Sie findet automatisch alle Ordner mit einer __init__.py
+    packages=find_packages(),
+    
+    # Fügen Sie hier alle Pakete hinzu, die Ihr Code braucht
+    install_requires=[
+        'torch',
+        'numpy',
+        'matplotlib',
+        'pykeops' 
+        # (Alle 'import ...' am Anfang Ihrer Skripte)
+    ],
+)
